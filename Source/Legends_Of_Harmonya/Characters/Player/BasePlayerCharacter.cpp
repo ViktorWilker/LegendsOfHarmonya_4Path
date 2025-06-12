76b2613d949fa9ac9cf2671359d3b2e7ec23735a
ViktorWilker <143961500+ViktorWilker@users.noreply.github.com>
2025-06-12 15:40:15 -0300

    create the basecharacter class

---
+++ Source/Legends_Of_Harmonya/Characters/Player/BasePlayerCharacter.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasePlayerCharacter.h"
+
+ABasePlayerCharacter::ABasePlayerCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABasePlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABasePlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABasePlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/Legends_Of_Harmonya/Characters/Player/BasePlayerCharacter.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Legends_Of_Harmonya/Characters/BaseCharacter.h"
+#include "BasePlayerCharacter.generated.h"
+
+UCLASS()
+class LEGENDS_OF_HARMONYA_API ABasePlayerCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABasePlayerCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
